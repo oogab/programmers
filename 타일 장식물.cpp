59c6363d10887c7cfb64e327859d06859c7d3e0e
백상욱 <oogab@naver.com>
2020-01-24 20:16:43 +0900

    타일 장식물.cpp
    programmers 코딩테스트 연습 > 동적계획법 > 타일 장식물
    c++ 언어로 작성된 코드

---
+++ 타일 장식물.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int N) {
+    long long answer = 0;
+    long long a = 1;
+    long long b = 1;
+    long long targetN;
+    
+    if (N==1)
+    {
+        answer = 4;
+        return answer;
+    }
+    
+    for(int i=2; i<N; i++) {
+        targetN = a+b;
+        a = b;
+        b = targetN;    
+    }
+    
+    answer = targetN*4 + a*2;
+    return answer;
+}
